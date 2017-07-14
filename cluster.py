a3a8b08b3605da33c08381692209069811b1a4b1
Jason Kai <kaitj@users.noreply.github.com>
2017-07-14 16:56:12 -0400

    Initial commit

---
+++ cluster.py
@@ -0,0 +1,108 @@
+""" cluster.py
+
+Module containing classes and functions used to cluster fibers and modify
+parameters pertaining to clusters.
+
+"""
+
+import numpy as np
+import fibers, distance, scalars
+from joblib import Parallel, delayed
+
+class Cluster:
+    """ Clustering of whole-brain tractography data from subject """ 
+    
+    def cluster(inputVTK, k_clusters, sigma, no_of_jobs):
+        """ 
+        Clustering of fibers based on pairwise fiber similarity 
+        
+            INPUT:
+                inputVTK - input polydata file
+                k_clusters - number of clusters via k-means clustering
+                sigma - width of kernel; adjust to alter sensitivity
+                no_of_jobs - processes to use to perform computation 
+        """         
+    
+        noFibers = inputVTK.GetNumberOfLines()    
+        if noFibers == 0:
+            print "ERROR: Input data has 0 fibers!"
+            return 
+        else:
+            print "Starting clustering..."
+            print "No. of fibers:", noFibers
+            print "No. of clusters:", k_clusters
+
+        
+def _pairwiseDistance_matrix(inputVTK, sigma, no_of_jobs):
+    """ An internal function used to compute an NxN distance matrix for all 
+    fibers (N) in the input data 
+    
+    INPUT:
+        inputVTK - input polydata file
+        sigma - width of kernel; adjust to alter sensitivity
+        no_of_jobs - processes to use to perform computation
+        
+    OUTPUT:
+        distances - NxN matrix containing distances between fibers     
+    """ 
+    
+    fiberArray = fibers.FiberArray()
+    fiberArray.convertFromVTK(inputVTK, pts_per_fiber=20)
+    
+    distances = Parallel(n_jobs=no_of_jobs, verbose=0)(
+            delayed(distance.fiberDistance)(fiberArray.getFiber(fidx),
+                    fiberArray)
+            for fidx in range(0, fiberArray.no_of_fibers))
+            
+    distances = np.array(distances)
+           
+    return distances
+
+def _pairwiseSimilarity_matrix(inputVTK, sigma, no_of_jobs):
+    """ An internal function used to compute an NxN similarity matrix for all
+    fibers (N) in the input data. 
+    
+    INPUT:
+        inputVTK - input polydata file
+        sigma - width of kernel; adjust to alter sensitivity
+        no_of_jobs - proccesses to use to perform computation
+        
+    OUTPUT:
+        similarity - NxN matrix containing similarity between fibers
+    """
+    
+    distances = _pairwiseDistance_matrix(inputVTK, sigma, no_of_jobs)
+    
+    sigmasq = np.square(sigma)
+    similarity = distance.gausKernel_similarity(distances, sigmasq)
+    
+    return similarity
+
+def _pairwiseQSimilarity_matrix(inputVTK, scalarData, scalarType, no_of_jobs): 
+    """ An internal function used to compute the cross-correlation between
+    quantitative metrics along a fiber. 
+    
+    INPUT:
+        inputVTK - input polydata file
+        no_of_jobs - processes to use to perform computation
+        
+    OUTPUT:
+        qSimilarity - NxN matrix containing correlation values between fibers
+    """ 
+    
+    fiberArray = fibers.FiberArray()
+    scalarArray = scalars.FiberArrayScalar()
+    
+    fiberArray.convertFromVTK(inputVTK, no_of_pts=20)
+    scalarArray.addScalar(inputVTK, fiberArray, scalarData, scalarType)
+    
+    qSimilarity = Parallel(n_jobs=no_of_jobs, verbose=0)(
+            delayed(np.corrcoef[0][1])(
+                    scalarArray.getScalar(fiberArray, scalarType)[fidx], 
+                    scalarArray)
+            for fidx in range(0, fiberArray.no_of_fibers))
+            
+    qSimilarity = np.array(qSimilarity)
+    
+    return qSimilarity
+        
---
+++ scalars.py
@@ -0,0 +1,141 @@
+""" scalar.py
+
+Module containing classes and functions related to fiber scalar information.
+
+"""
+
+import numpy as np
+import vtk
+from collections import defaultdict
+
+def tree():
+    """ Creates tree to store quantitative information
+    
+    INPUT:
+        none
+        
+    OUTPUT:
+        none
+    """
+    
+    return defaultdict(tree)
+
+class FiberScalar:
+    """ Scalar information pertaining to a single fiber """
+    
+    def __init__(self):
+        self.tree_scalar = tree()
+        
+    def getReverseScalar(self, fiberArray):
+        """ Determine the scalar quantity corresponding to reverse order of 
+        the fiber.
+        
+        INPUT:
+            none
+            
+        OUTPUT:
+            none
+        """
+        
+        fiberScalar = FiberScalar()
+        
+        fiberScalar.pts_per_fiber = fiberArray.pts_per_fiber
+
+        for pidx in range(0, fiberArray.pts_per_fiber):
+            fiberScalar.tree_scalar[0][pidx] = \
+                self.tree_scalar[0][fiberArray.pts_per_fiber - (pidx+1)]
+                
+        return fiberScalar
+    
+class FiberArrayScalar:
+    """ Scalar information pertaining to a group of fibers. 
+    Value returned is of class FiberScalar. """
+    
+    def __init__(self):
+        self.fiberTree_scalar = tree()
+        
+    def _calc_fiber_indices(self, fiberLength, pts_per_fiber):
+        """ *INTERNAL FUNCTION*
+        Determine indices to traverse data along a fiber.
+        
+        Indices include both end points of the fiber plus evenly spaced points
+        along the line. Module determines which indices are wanted based on 
+        fiber length and desired number of points along the length.
+        
+        INPUT:
+            fiberLength - Number of points along a fiber
+            pts_per_length - Number of desired points along fiber
+            
+        OUTPUT:
+            idxList - Corresponding new indices to traverse along fiber
+        """
+        
+        # Step length between points along fiber
+        stepLength = (fiberLength - 1.0) / (pts_per_fiber - 1.0)
+            
+        # Output indices along fiber
+        idxList = []
+        for idx in range(0, pts_per_fiber):
+            idxList.append(idx * stepLength)
+            
+        return idxList
+        
+    def addScalar(self, inputVTK, fiberArray, scalarData, scalarType):
+        """ Add scalar information pertaining to tractography. Values are 
+        stored with a tree format. This function is dynamic and can add new 
+        quantitative measurements as needed.
+        
+        INPUT:
+            inputVTK - Tractography polydata to extract corresponding indices
+            fiberArray - Array of fibers to add scalar to
+            scalarData - List of scalar values to be stored
+            scalarType - Type of quantitative scalar (ie. FA, T1)
+            
+        OUTPUT:
+            none
+        """
+        
+        inputVTK.GetLines().InitTraversal()
+        ptIds = vtk.vtkIdList()
+        
+        # Loop over all fibers
+        for fidx in range(0, fiberArray.no_of_fibers):
+            inputVTK.GetLines().GetNextCell(ptIds)
+            fiberLength = ptIds.GetNumberOfIds()
+            
+            # Loop over pts for ea. fiber
+            pidx = 0
+            for lineIdx in self._calc_fiber_indices(fiberLength, 
+                                                    fiberArray.pts_per_fiber):
+                
+                # Find point index
+                ptidx = ptIds.GetId(int(round(lineIdx)))
+                
+                self.fiberTree_scalar[fidx][pidx][scalarType] = \
+                    scalarData[ptidx]
+                    
+                pidx += 1               
+
+    def getScalar(self, fiberArray, scalarType):
+        """ Extracts scalar information of a speficied scalarType pertaining to
+        a single fiber.
+        
+        INPUT:
+            fiberArray - Array of fibers to get scalar from
+            scalarType - Type of quantitative scalar (ie. FA, T1)
+            
+        OUTPUT:
+            scalarList - List of scalar vlaues indexed by fiber and point 
+        """
+        
+        scalarList = np.zeros((fiberArray.no_of_fibers,
+                                      fiberArray.pts_per_fiber))
+                
+        for fidx in range(0, fiberArray.no_of_fibers):
+            for pidx in range(0, fiberArray.pts_per_fiber):
+                
+                scalarValue = \
+                    self.fiberTree_scalar[fidx][pidx][scalarType]
+                scalarList[fidx][pidx] = float(scalarValue)
+                
+        return scalarList
