fc938d3f516e2df8eaaeef1d1a564b3d2faac9a1
Jason Kai <tkai@uwo.ca>
2017-07-15 20:03:08 -0400

    updated cluster and distance modules

--- cluster.py
+++ cluster.py
@@ -10,99 +10,98 @@ import fibers, distance, scalars
 from joblib import Parallel, delayed
 
 class Cluster:
-    """ Clustering of whole-brain tractography data from subject """ 
-    
+    """ Clustering of whole-brain tractography data from subject """
+
     def cluster(inputVTK, k_clusters, sigma, no_of_jobs):
-        """ 
-        Clustering of fibers based on pairwise fiber similarity 
-        
+        """
+        Clustering of fibers based on pairwise fiber similarity
+
             INPUT:
                 inputVTK - input polydata file
                 k_clusters - number of clusters via k-means clustering
                 sigma - width of kernel; adjust to alter sensitivity
-                no_of_jobs - processes to use to perform computation 
-        """         
-    
-        noFibers = inputVTK.GetNumberOfLines()    
+                no_of_jobs - processes to use to perform computation
+        """
+
+        noFibers = inputVTK.GetNumberOfLines()
         if noFibers == 0:
             print "ERROR: Input data has 0 fibers!"
-            return 
+            return
         else:
             print "Starting clustering..."
             print "No. of fibers:", noFibers
             print "No. of clusters:", k_clusters
 
-        
+
 def _pairwiseDistance_matrix(inputVTK, sigma, no_of_jobs):
-    """ An internal function used to compute an NxN distance matrix for all 
-    fibers (N) in the input data 
-    
+    """ An internal function used to compute an NxN distance matrix for all
+    fibers (N) in the input data
+
     INPUT:
         inputVTK - input polydata file
         sigma - width of kernel; adjust to alter sensitivity
         no_of_jobs - processes to use to perform computation
-        
+
     OUTPUT:
-        distances - NxN matrix containing distances between fibers     
-    """ 
-    
+        distances - NxN matrix containing distances between fibers
+    """
+
     fiberArray = fibers.FiberArray()
     fiberArray.convertFromVTK(inputVTK, pts_per_fiber=20)
-    
+
     distances = Parallel(n_jobs=no_of_jobs, verbose=0)(
             delayed(distance.fiberDistance)(fiberArray.getFiber(fidx),
                     fiberArray)
             for fidx in range(0, fiberArray.no_of_fibers))
-            
+
     distances = np.array(distances)
-           
+
     return distances
 
 def _pairwiseSimilarity_matrix(inputVTK, sigma, no_of_jobs):
     """ An internal function used to compute an NxN similarity matrix for all
-    fibers (N) in the input data. 
-    
+    fibers (N) in the input data.
+
     INPUT:
         inputVTK - input polydata file
         sigma - width of kernel; adjust to alter sensitivity
         no_of_jobs - proccesses to use to perform computation
-        
+
     OUTPUT:
         similarity - NxN matrix containing similarity between fibers
     """
-    
-    distances = _pairwiseDistance_matrix(inputVTK, sigma, no_of_jobs)
-    
-    sigmasq = np.square(sigma)
-    similarity = distance.gausKernel_similarity(distances, sigmasq)
-    
-    return similarity
 
-def _pairwiseQSimilarity_matrix(inputVTK, scalarData, scalarType, no_of_jobs): 
+    distances = _pairwiseDistance_matrix(inputVTK, sigma, no_of_jobs)
+
+    sigmasq = np.square(sigma)
+    similarities = distance.gausKernel_similarity(distances, sigmasq)
+
+    return similarities
+
+def _pairwiseQSimilarity_matrix(inputVTK, scalarData, scalarType, no_of_jobs):
     """ An internal function used to compute the cross-correlation between
-    quantitative metrics along a fiber. 
-    
+    quantitative metrics along a fiber.
+
     INPUT:
         inputVTK - input polydata file
         no_of_jobs - processes to use to perform computation
-        
+
     OUTPUT:
         qSimilarity - NxN matrix containing correlation values between fibers
-    """ 
-    
+    """
+
     fiberArray = fibers.FiberArray()
     scalarArray = scalars.FiberArrayScalar()
-    
+
     fiberArray.convertFromVTK(inputVTK, no_of_pts=20)
     scalarArray.addScalar(inputVTK, fiberArray, scalarData, scalarType)
-    
+
     qSimilarity = Parallel(n_jobs=no_of_jobs, verbose=0)(
             delayed(np.corrcoef[0][1])(
-                    scalarArray.getScalar(fiberArray, scalarType)[fidx], 
+                    scalarArray.getScalar(fiberArray, scalarType)[fidx],
                     scalarArray)
             for fidx in range(0, fiberArray.no_of_fibers))
-            
+
     qSimilarity = np.array(qSimilarity)
-    
+
     return qSimilarity
-        
--- distance.py
+++ distance.py
@@ -9,74 +9,75 @@ import numpy as np
 
 def _fiberDistance_internal(fiber, fiberArray):
     """ Computes the distance between one fiber and individual fibers within a
-    group (array) of fibers using MeanSquared method. 
-    
+    group (array) of fibers using MeanSquared method.
+
     INPUT:
         fiber - single fiber to be compared
         fiberArray - group of fibers that lone fiber is to be compared to
-        
+
     OUTPUT:
         distance - computed distance from single fiber to those within fiber
         group
     """
-    
+
     # Calculates the distance between points of each fiber
     dx = fiberArray.fiberArray_x - fiber.x
     dy = fiberArray.fiberArray_y - fiber.y
-    dz = fiberArray.fiberArray_z - fiber.z 
-    
+    dz = fiberArray.fiberArray_z - fiber.z
+
     # Squares calculated distances
     dx_sq = np.square(dx)
     dy_sq = np.square(dy)
     dz_sq = np.square(dz)
-    
-    # Sums the squared values of the distances in each axis 
+
+    # Sums the squared values of the distances in each axis
     distance = dx_sq + dy_sq + dz_sq
-    
-    # Calculates distances between each fiber
+
+    # Sum along fiber and normalize
     distance = np.sum(distance, 1)
-    
+    distance = distance / float(fiberArray.pts_per_fiber)
+
     return distance
 
 def fiberDistance(fiber, fiberArray):
-    """ Computes the distance between one fiber and individual fibers within a 
+    """ Computes the distance between one fiber and individual fibers within a
     group (array) of fibers. This function also handles equivalent fiber
     representations. 
-    
+
     INPUT:
         fiber - single fiber to be compared
         fiberArray - group of fibers that lone fiber is to be compared to
-        
+
     OUTPUT:
         distance - minimum distance between group of fiber and single fiber
         traversed in both directions
     """
-    
+
     # Get same fiber in reverse order
     fiberEquiv = fiber.getReverseFiber()
-    
+
     # Compute distances for fiber and fiber equivalent to fiber group
     distance1 = _fiberDistance_internal(fiber, fiberArray)
     distance2 = _fiberDistance_internal(fiberEquiv, fiberArray)
-    
+
     # Minimum distance more likely to be part of cluster; return distance
     distance = np.minimum(distance1, distance2)
-    
+
     return distance
 
-def gausKernel_similarity(distance, sigmasq):
+def gausKernel_similarity(distance, sigmasq=100):
     """ Computes the similarity using a Gaussian kernel
-    
+
     INPUT:
         distance - Euclidean distance between points
         sigma - width of the kernel; adjust to alter sensitivity
-        
+
     OUTPUT:
         similiarities - scalar values pertaining to the similarity of fiber to
         group of fibers (0 is dissimilar, 1 is identical)
     """
-    
+
     # Computes similarity using a Gaussian kernel
-    similarities = np.exp(-distance / sigmasq)
-    
+    similarities = np.exp(-distance / (sigmasq))
+
     return similarities
