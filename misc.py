be4c1b70d3611da3f73cf6d135de21885e176112
Jason Kai <tkai@uwo.ca>
2017-08-21 15:08:26 -0400

    added function to save image matrices

--- misc.py
+++ misc.py
@@ -6,6 +6,8 @@ in other modules.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def corr(data1, data2):
     """ Perform correlation coefficient calculation using numpy library.
@@ -24,3 +26,21 @@ def corr(data1, data2):
     val = val[0][1]
 
     return val
+
+    def saveMatrix(dirpath, matrix, matrixType, colormap='viridis'):
+        """ Function used to save matrices.
+              Defaults to 'viridis' colormap.
+        """
+
+        f = plt.figure(figsize=(10, 10))
+
+        ax = plt.gca()
+        ax.tick_params(axis='both', labelsize=14)
+        div = make_axes_locatable(ax)
+        cax = div.append_axes('right', size='5%', pad=0.25)
+        cax.tick_params(labelsize=14)
+
+        im = plt.imshow(matrix, cmap=colormap)
+        plt.title((matrixType + 'Similarity'), fontsize=16)
+        plt.colorbar(im, cax)
+        plt.savefig(dirpath + '/' + matrixType.lower() + 'Similarity.png')
