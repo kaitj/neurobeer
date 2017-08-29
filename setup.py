00430d5d81917a22c176607a91638c647edd8071
Jason Kai <tkai@uwo.ca>
2017-08-29 14:29:28 -0400

    added code for packaging of tool

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Project information
+    name='Tractography',
+    version='0.1.0',
+    packages=find_packages(),
+
+    # Metadata
+    author='Jason Kai',
+    author_email='tkai@uwo.ca',
+    description='Automated clustering and evaluation tool for white matter tractography',
+    url='https://github.com/kaitj/tractography',
+)
---
+++ tractography/__init__.py
@@ -0,0 +1,20 @@
+"""
+The package provides a number of modules to be used in the clustering,
+extraction, and evaluation of white matter tractography.
+
+"""
+
+print 'Importing tractography clustering package...'
+
+dependencies = ['joblib', 'numpy', 'matplotlib', 'scipy', 'sklearn', 'vtk']
+modules = ['cluster', 'distance', 'fibers', 'misc', 'scalars', 'stats', 'tractio']
+
+for dep in dependencies:
+    try:
+        exec('import %s' % dep)
+    except ImportError:
+        print '%s is not installed. Functionality of this package will be unavailable..' % dep
+        print 'Please installl %s to use this package.' % dep
+
+for module in modules:
+    exec('import %s' % module)
