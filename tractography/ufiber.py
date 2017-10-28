0c262730148f21333efd29f54aa1e834a0edabb0
Jason Kai <tkai@uwo.ca>
2017-10-28 11:03:28 -0400

    added module for ufiber processing

---
+++ tractography/ufiber.py
@@ -0,0 +1,117 @@
+""" ufiber.py
+
+Module containing classes and functions used to identify and process
+ufibers
+
+"""
+
+import numpy as np
+import fibers
+
+def findUFiber(fiberData):
+    """
+    Identifies U-fibers from tractography
+
+    INPUT:
+        fiberData - Fiber tree containing tractography data
+
+    OUTPUT:
+        uArray - Array containing indices of all u-shaped fibers
+    """
+    # Array storing indices of u-shaped fibers
+    uArray = []
+
+    # Determine fiber length and u-shape
+    for fidx in range(fiberData.no_of_fibers):
+        L, fl = _calcFiberLength(fiberData, fidx)
+
+        D = _calcEndPointSep(fiberData, fidx)
+
+        if fl is True:
+            continue
+        elif (L > 30) and (D <= (L / np.pi)) and (L < 80):
+            uArray.append(fidx)
+
+    return uArray
+
+def extractUFiber(fiberData, uArray):
+    """
+    Extracts u-shaped fibers of class FiberTree
+
+    INPUT:
+        fiberData - Fiber tree containing tractography information
+        uArray - Array of indices containing u-shaped fibers
+
+    OUTPUT:
+        uFiberTree - Fiber tree instance containing only u-shaped fibers
+    """
+
+    uFiberTree = fiberData.getFibers(uArray)
+    uFiberTree = fibers.convertFromTuple(uFiberTree)
+
+    return uFiberTree
+
+def _calcFiberLength(fiberData, fidx):
+    """ * INTERNAL FUNCTION *
+    Calculates the fiber length via arc length
+
+    INPUT:
+        fiberData - Fiber tree copython popntaining tractography information
+        fidx - Fiber index
+
+    OUTPUT:
+        L - Length of fiberlen(uArray)
+    """
+    no_of_pts = fiberData.pts_per_fiber
+
+    if no_of_pts < 2:
+        print "Not enouguFiberData.fiberTreeh samples to determine length of fiber"
+        raise ValueError
+
+    L = 0
+
+    for idx in range(1, no_of_pts):
+        x1 = fiberData.fiberTree[fidx][idx]['x']
+        x2 = fiberData.fiberTree[fidx][idx - 1]['x']
+        y1 = fiberData.fiberTree[fidx][idx]['y']
+        y2 = fiberData.fiberTree[fidx][idx - 1]['y']
+        z1 = fiberData.fiberTree[fidx][idx]['z']
+        z2 = fiberData.fiberTree[fidx][idx - 1]['z']
+
+        # Temporary fix for finding if fibers cross
+        if x1 < 0 and x2 > 0:
+            fl = True
+            return 0, fl
+        elif x1 > 0 and x2 < 0:
+            fl = True
+            return 0, fl
+        else:
+            fl = False
+
+        L = L + np.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+    return L, fl
+
+def _calcEndPointSep(fiberData, fidx):
+    """ * INTERNAL uFiberData.fiberTreeFUNCTION *
+    Calculates distance between end points
+
+    INPUT:
+        fiberData - Fiber tree containing tractography information
+        fidx - Fiber index
+
+    OUTPUT:
+        D - length of fiber
+    """
+    endpt = fiberData.pts_per_fiber - 1
+
+    x1 = fiberData.fiberTree[fidx][0]['x']
+    x2 = fiberData.fiberTree[fidx][endpt]['x']
+    y1 = fiberData.fiberTree[fidx][0]['y']
+    y2 = fiberData.fiberTree[fidx][endpt]['y']
+    z1 = fiberData.fiberTree[fidx][0]['z']
+    z2 = fiberData.fiberTree[fidx][endpt]['z']
+
+    D = np.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+    return D
