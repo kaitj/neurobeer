f7ff1b270359915a06f7aea78b246b77c158e590
kaitj <tkai@uwo.ca>
2018-08-27 13:32:46 -0400

    update to dev branch and fix export of vasst-dev

--- singularity/install_scripts/05_install_neurobeer.sh
+++ singularity/install_scripts/05_install_neurobeer.sh
@@ -10,6 +10,7 @@ apt-get install -y python3-tk
 # Git
 git clone https://github.com/kaitj/neurobeer $DIR/neurobeer
 cd $DIR/neurobeer
+git checkout khanlab # dev branch, remove for release
 
 # Install requirements
 pip3 install -r requirements.txt
