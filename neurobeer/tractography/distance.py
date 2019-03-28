4a81a5bfc4cc94358441d3e2b911d8b1fdce9fbc
kaitj <tkai@uwo.ca>
2019-03-28 10:48:34 -0400

    updated distance calculation

--- neurobeer/tractography/distance.py
+++ neurobeer/tractography/distance.py
@@ -28,30 +28,22 @@ def _fiberDistance_internal(fiberMatrix1, fiberMatrix2, flip=False):
     # Calculates the squared distance between fibers
     if flip is False:
         for i in range(fiberMatrix1.shape[1]):
-            distance[i] = np.sqrt((np.linalg.norm(fiberMatrix1[0, i, :] -
-                                                  fiberMatrix2[0, :, :],
-                                                  axis=1))**2 +
-                                  (np.linalg.norm(fiberMatrix1[1, i, :] -
-                                                  fiberMatrix2[1, :, :],
-                                                  axis=1))**2 +
-                                  (np.linalg.norm(fiberMatrix1[2, i, :] -
-                                                  fiberMatrix2[2, :, :],
-                                                  axis=1))**2)
+            distance[i] = np.mean(np.sqrt((fiberMatrix1[0, i, :] -
+                                           fiberMatrix2[0, :, :])**2 +
+                                          (fiberMatrix1[1, i, :] -
+                                           fiberMatrix2[1, :, :])**2 +
+                                          (fiberMatrix1[2, i, :] -
+                                           fiberMatrix2[2, :, :])**2), axis=1)
     # Flipped fiber
     else:
         for i in range(fiberMatrix1.shape[1]):
-            distance[i] = np.sqrt((np.linalg.norm(
-                                   np.flip(fiberMatrix1[0, i, :], axis=0) -
-                                           fiberMatrix2[0, :, :],
-                                           axis=1))**2 +
-                                  (np.linalg.norm(
-                                   np.flip(fiberMatrix1[1, i, :], axis=0) -
-                                           fiberMatrix2[1, :, :],
-                                           axis=1))**2 +
-                                  (np.linalg.norm(
-                                   np.flip(fiberMatrix1[2, i, :], axis=0) -
-                                           fiberMatrix2[2, :, :],
-                                           axis=1))**2)
+            distance[i] = np.mean(np.sqrt(
+                                  (np.flip(fiberMatrix1[0, i, :], axis=0) -
+                                           fiberMatrix2[0, :, :])**2 +
+                                  (np.flip(fiberMatrix1[1, i, :], axis=0) -
+                                           fiberMatrix2[1, :, :])**2 +
+                                  (np.flip(fiberMatrix1[2, i, :], axis=0) -
+                                           fiberMatrix2[2, :, :])**2), axis=1)
 
         del fiberMatrix1, fiberMatrix2
 
