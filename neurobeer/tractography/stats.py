ef8d066a168487a12a2eb010a16cc90daf700561
kaitj <tkai@uwo.ca>
2020-02-19 14:21:22 -0500

    remove plotting of stats (unused)

--- neurobeer/tractography/stats.py
+++ neurobeer/tractography/stats.py
@@ -7,7 +7,6 @@ tract-based statistics.
 
 import os, csv
 import numpy as np
-import matplotlib.pyplot as plt
 
 def _mean(fiberTree, scalarType, idxes=None):
     """ *INTERNAL FUNCTION*
@@ -176,59 +175,3 @@ def writeCSV(clusterLabel, fiberTree, scalarType, idxes=None, dirpath=None):
         writer.writerow(rowInfo)
 
     f.close()
-
-def plotStats(fiberTree, scalarType, idxes=None, dirpath=None):
-    """
-    Plots the calculated tract-based statistics for each fiber bundle
-
-    INPUT:
-        fiberTree - tree containing spatial and quantitative information of
-                    fibers
-        scalarType - type of quantitative data to plot
-        idxes - indices to extract info from; defaults None (returns data for
-                all fibers)
-        dirpath - location to store plots; defaults None
-
-    OUTPUT:
-        none
-    """
-
-    if dirpath is None:
-        dirpath = os.getcwd()
-    else:
-        if not os.path.exists(dirpath):
-            os.makedirs(dirpath)
-
-    # Statistical calculations for plot
-    x = range(fiberTree.pts_per_fiber)
-    yavg, avg = _mean(fiberTree, scalarType, idxes)
-    ystd, stdev = _stddev(fiberTree, scalarType, idxes)
-
-    # Plot of stats
-    f = plt.figure(figsize=(10, 10))
-    plt.grid()
-
-    avgline = plt.plot(x, yavg, 'b', linewidth=3, label='Mean')
-    avgdot = plt.plot(x, yavg, '.r', markersize=15)
-    stdupper = plt.plot(x, yavg+ystd, 'k', linewidth=2, alpha=0.7)
-    stdlower = plt.plot(x, yavg-ystd, 'k', linewidth=2, alpha=0.7,
-                        label='Std. Dev')
-    plt.fill_between(x, yavg-ystd, yavg+ystd, facecolor='grey', alpha=0.7)
-    plt.xlim(min(x), max(x))
-
-    # Plot labels
-    fileName = scalarType.split('/', -1)[-1]
-    title = fileName + ' (%.2f +/- %.2f)' % (avg, stdev)
-    ytitle = fileName
-
-    plt.title(title, fontsize=16)
-    plt.ylabel(ytitle.upper(), fontsize=14)
-    plt.xlabel('Fiber Samples', fontsize=14)
-    plt.legend(loc='upper left', fontsize=14)
-    plt.tick_params(axis='both', which='major', labelsize=14,
-                    length=10, pad=10, width=1, direction='out',
-                    top='off', right='off')
-
-    # Save figure
-    plt.savefig(dirpath + '/' + fileName + '_stats.png')
-    plt.close(f)
